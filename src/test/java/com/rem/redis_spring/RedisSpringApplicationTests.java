1deb93c51d3d9a1fb01b833276bea40dd0fba572
aladagramazan <ramazan.aladag@yaani.com>
2024-08-21 12:12:22 +0300

    first commit

---
+++ src/test/java/com/rem/redis_spring/RedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rem.redis_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
